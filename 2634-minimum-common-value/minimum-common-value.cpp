a12dc5d8f6c8d581ba1f9fc0a8bc94a5d14185c5
AkshayTiwari27 <131443493+AkshayTiwari27@users.noreply.github.com>
2025-01-10 20:23:18 +0530

    Time: 0 ms (100.00%) | Memory: 54.4 MB (68.53%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        set<int>st(nums2.begin(),nums2.end());
-        int ans=-1;
+        int i=0,j=0;
+        int n=nums1.size();
+        int m=nums2.size();
 
-        for(int i=0;i<nums1.size();i++){
-            if(st.find(nums1[i])!=st.end()){
-                ans=nums1[i];
-                break;
-            }
+        while(i<n&& j<m){
+            if(nums1[i]==nums2[j]) return nums1[i];
+            else if(nums1[i]<nums2[j]) i++;
+            else j++;
         }
 
-        return ans;
+        return -1;
     }
 };
