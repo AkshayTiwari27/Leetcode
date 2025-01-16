13ce47e9f99a153d0a6842f0205cba434bd199be
AkshayTiwari27 <131443493+AkshayTiwari27@users.noreply.github.com>
2025-01-17 01:18:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (16.17%) - LeetSync

--- 2509-minimize-xor/minimize-xor.cpp
+++ 2509-minimize-xor/minimize-xor.cpp
@@ -1,19 +1,46 @@
 class Solution {
 public:
+    bool setbits(int &x, int bit) {
+        if (bit >= 0 && bit < 32) {
+            x |= (1 << bit);
+            return true;
+        }
+        return false;
+    }
+
+    bool issetbits(int &x, int bit) {
+        if (bit >= 0 && bit < 32) {
+            return x & (1 << bit);
+        }
+        return false;
+    }
+
+    bool unsetbits(int &x, int bit) {
+        if (bit >= 0 && bit < 32) {
+            x &= ~(1 << bit);
+            return true;
+        }
+        return false;
+    }
+
     int minimizeXor(int num1, int num2) {
-        int a = __builtin_popcount(num1);
-        int b = __builtin_popcount(num2);
-        int res = num1;
-        for (int i = 0; i < 32; ++i) {
-            if (a > b && (1 << i) & num1) {
-                res ^= 1 << i;
-                --a;
-            }
-            if (a < b && !((1 << i) & num1)) {
-                res ^= 1 << i;
-                ++a;
+        int x = 0;
+        int req_setbits = __builtin_popcount(num2);
+        
+        for(int bit=31;bit>=0 && req_setbits>0;bit--){
+            if(issetbits(num1,bit)){
+                setbits(x,bit);
+                req_setbits--;
             }
         }
-        return res;
+
+        for(int bit=0;bit<32 && req_setbits>0;bit++){
+            if(!issetbits(x,bit)){
+                setbits(x,bit);
+                req_setbits--;
+            }
+        }
+
+        return x;
     }
 };
