549d42437925c537c336447bd7684bdf116f5fd5
AkshayTiwari27 <131443493+AkshayTiwari27@users.noreply.github.com>
2025-01-20 23:46:41 +0530

    Time: 98 ms (58.84%) | Memory: 171.1 MB (28.35%) - LeetSync

--- 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
-        unordered_map<int,int>mp;
         int m=mat.size();
         int n=mat[0].size();
+        unordered_map<int,pair<int,int>>mp;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                int val=mat[i][j];
+                mp[val]={i,j};
+            }
+        }
+        vector<int>rows(m,0),cols(n,0);
         for(int i=0;i<arr.size();i++){
-            mp[arr[i]]=i;
-        }
-        int minIndex=INT_MAX;
-        for(int row=0;row<m;row++){
-            int lastIndex=INT_MIN;
-            for(int col=0;col<n;col++){
-                int val=mat[row][col];
-                int idx=mp[val];
-                lastIndex=max(lastIndex,idx);
-            }
-            minIndex=min(lastIndex,minIndex);
-        }
-        
-        for(int col=0;col<n;col++){
-            int lastIndex=INT_MIN;
-            for(int row=0;row<m;row++){
-                int val=mat[row][col];
-                int idx=mp[val];
-                lastIndex=max(idx,lastIndex);
-            }
-            minIndex=min(minIndex,lastIndex);
+            int val=arr[i];
+            auto [row,col]=mp[val];
+            rows[row]++;
+            cols[col]++;
+            if(rows[row]==n || cols[col]==m) return i;
         }
 
-        return minIndex;
+        return -1;
     }
 };
