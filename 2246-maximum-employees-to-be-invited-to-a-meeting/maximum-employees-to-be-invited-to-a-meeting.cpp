7a3afa1d9a270f0bebeb390e363fb21d2537dca9
AkshayTiwari27 <131443493+AkshayTiwari27@users.noreply.github.com>
2025-01-27 00:07:08 +0530

    Time: 656 ms (15.58%) | Memory: 339.8 MB (5.33%) - LeetSync

--- 2246-maximum-employees-to-be-invited-to-a-meeting/maximum-employees-to-be-invited-to-a-meeting.cpp
+++ 2246-maximum-employees-to-be-invited-to-a-meeting/maximum-employees-to-be-invited-to-a-meeting.cpp
@@ -1,78 +1,66 @@
 class Solution {
 public:
+    int bfs(int start,unordered_map<int,vector<int>>adj,vector<bool>&visited){
+        int maxDistance=0;
+        queue<pair<int,int>>q;
+        q.push({start,0});
+        visited[start] = true;
 
-    int BFS(int start, unordered_map<int, vector<int>>& adj, vector<bool>& visited) {
-        queue<pair<int, int>> que; //{node, path length}
-        que.push({start, 0});
-        int maxDistance = 0;
-
-        while(!que.empty()) {
-            auto[currNode, dist] = que.front();
-            que.pop();
-
-            for(auto &ngbr : adj[currNode]) {
-                if(!visited[ngbr]) {
-                    visited[ngbr] = true;
-                    que.push({ngbr, dist+1});
-                    maxDistance = max(maxDistance, dist+1);
+        
+        while(!q.empty()){
+            auto [currNode,dist]=q.front();
+            q.pop();
+            for(auto& v:adj[currNode]){
+                if(!visited[v]){
+                    visited[v]=true;
+                    q.push({v,dist+1});
+                    maxDistance=max(maxDistance,dist+1);
                 }
             }
         }
+
 
         return maxDistance;
     }
-
-    int maximumInvitations(vector<int>& favorite) {
-        int n = favorite.size();
-        unordered_map<int, vector<int>> adj;
-
-        for(int i = 0; i < n; i++) {
-            int u = i;
-            int v = favorite[i];
-            // u --> v
-            adj[v].push_back(u); //reversed graph - so that we can find the path length after traversal
+    int maximumInvitations(vector<int>& favourite) {
+        int n=favourite.size();
+        unordered_map<int,vector<int>>adj;
+        for(int i=0;i<n;i++){
+            int u=i;
+            int v=favourite[i];
+            adj[v].push_back(u);
         }
 
-        int longestCycleEmplCount = 0;
-        int happyCoupleEmplCount  = 0; //cycle length = 2 waalo se kitna milpaega total
-
-        vector<bool> visited(n, false);
-
-        for(int i = 0; i < n; i++) {
-
-            if(!visited[i]) {
-                //{node, abtak ka node count}
-                unordered_map<int, int> mp;
-
-                int currNode      = i;
-                int currNodeCount = 0;
-
-                while(!visited[currNode]) { //until cycle is not detected
-                    visited[currNode] = true;
-                    mp[currNode] = currNodeCount;
-
-                    int nextNode = favorite[currNode]; //favorite node of curr node
-                    currNodeCount += 1;
-
-                    if(mp.count(nextNode)) { //already visited hai ye. Matlab cycle detect hogaya hai
-                        int cycleLength = currNodeCount - mp[nextNode];
-                        longestCycleEmplCount = max(longestCycleEmplCount, cycleLength);
-
-                        if(cycleLength == 2) { //happy couple case
-                            //currNode <-> nextNode = 2 nodes
-                            vector<bool> visitedNodes(n, false);
-                            visitedNodes[currNode] = true;
-                            visitedNodes[nextNode] = true;
-                            happyCoupleEmplCount += 2 + BFS(currNode, adj, visitedNodes) + BFS(nextNode, adj, visitedNodes);
+        int happyCount=0;
+        int maxLength=0;
+        vector<bool>visited(n,false);
+        
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                unordered_map<int,int>mp;
+                int currNode=i;
+                int currCount=0;
+                while(!visited[currNode]){
+                    visited[currNode]=true;
+                    mp[currNode]=currCount;
+                    int nextNode=favourite[currNode];
+                    currCount+=1;
+                    if(mp.count(nextNode)){
+                        int length=currCount-mp[nextNode];
+                        maxLength=max(maxLength,length);
+                        if(length==2){
+                            vector<bool>visitedNodes(n,false);
+                            visitedNodes[currNode]=true;
+                            visitedNodes[nextNode]=true;
+                            happyCount+=2+bfs(currNode,adj,visitedNodes)+bfs(nextNode,adj,visitedNodes);
                         }
                         break;
                     }
-                    currNode = nextNode;
+                    currNode=nextNode;
                 }
             }
         }
 
-        return max(happyCoupleEmplCount, longestCycleEmplCount);
-
+        return max(happyCount,maxLength);
     }
 };
