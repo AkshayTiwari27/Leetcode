8ca8c9964a76ed186642c9ba6a0a933b9873fc2e
AkshayTiwari27 <131443493+AkshayTiwari27@users.noreply.github.com>
2025-02-11 10:15:51 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (7.07%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-        while(s.find(part)!=string::npos){
-            s.erase(s.find(part),part.length());
+        string result;
+        for(int i=0; i<s.length();i++){
+            result.push_back(s[i]);
+            if(result.size() >= part.length() && result.substr(result.length() - part.length()) == part){
+                result.erase(result.length() - part.length(), part.length());
+            }
         }
 
-        return s;
+        return result;
     }
 };
