75c1784b1db7072a08ca7b10699cade97c0c6be1
AkshayTiwari27 <131443493+AkshayTiwari27@users.noreply.github.com>
2025-02-21 20:11:56 +0530

    Time: 7 ms (82.43%) | Memory: 27.7 MB (74.21%) - LeetSync

--- 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.cpp
+++ 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.cpp
@@ -12,17 +12,32 @@
 class FindElements {
 public:
     set<int>st;
-    void dfs(TreeNode* root, int x){
-        if(!root) return;
+    
+void bfs(TreeNode* root, int x) {
+    queue<TreeNode*> q;
+    root->val = x;
+    q.push(root);
 
-        root->val = x;
-        st.insert(x);
-        dfs(root->left, 2*x + 1);
-        dfs(root->right, 2*x + 2);
+    while (!q.empty()) {
+        TreeNode* temp = q.front();
+        q.pop();
+
+        st.insert(temp->val);
+
+        if (temp->left) {
+            temp->left->val = 2 * temp->val + 1;
+            q.push(temp->left);
+        }
+        if (temp->right) {
+            temp->right->val = 2 * temp->val + 2;
+            q.push(temp->right);
+        }
     }
+}
+
     FindElements(TreeNode* root) {
         st.clear();
-        dfs(root, 0);
+        bfs(root, 0);
     }
     
     bool find(int target) {
