4c9c04af5a5750968e2aa4159b610eae4559f51d
AkshayTiwari27 <131443493+AkshayTiwari27@users.noreply.github.com>
2025-03-12 23:43:34 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (78.58%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
-    int dp[101];
-    int solve(int i, vector<int>& nums){
-        if(i < 0) return 0;
-        if(dp[i] != -1) return dp[i];
-        return dp[i] = max(nums[i] + solve(i-2, nums), solve(i-1, nums));
-    }
+    
     int rob(vector<int>& nums) {
         int n = nums.size();
-        memset(dp, -1, sizeof(dp));
-        return solve(n-1, nums);
+        
+        vector<int>dp(101, 0);
+        if(n == 1) return nums[0];
+        dp[0] = nums[0];
+        dp[1] = max(nums[1],nums[0]);
+        for(int i = 2; i<n; i++){
+            dp[i] = max(nums[i] + dp[i-2], dp[i-1]);
+        }
+
+        return dp[n-1];
     }
 };
