1a53dbb9bfe09e005ced2b22de3e4bb9fbb1d343
AkshayTiwari27 <131443493+AkshayTiwari27@users.noreply.github.com>
2025-03-17 22:53:08 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (95.23%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -2,30 +2,28 @@ class Solution {
 public:
     int n;
     int dp[366];
-    int solve(vector<int>& days, vector<int>& costs, int i){
-        if(i >= days.size()) return dp[i] = 0;
-
-        if(dp[i] != -1) return dp[i];
-
-        int cost1 = costs[0] + solve(days, costs, i + 1);
-
-        int j = i;
-        while(j < n && days[j] < days[i] + 7){
-            j++;
-        }
-        int cost7 = costs[1] + solve(days, costs, j);
-
-        j = i;
-        while(j < n && days[j] < days[i] + 30){
-            j++;
-        }
-        int cost30 = costs[2] + solve(days, costs, j);
-
-        return dp[i] = min({cost1, cost7, cost30});
-    }
+    
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         n = days.size();
-        memset(dp, -1, sizeof(dp));
-        return solve(days, costs, 0);
+        memset(dp, 0, sizeof(dp));
+        for(int i = n-1; i>= 0; i--){
+            int cost1 = costs[0] + dp[i + 1];
+
+            int j = i;
+            while(j < n && days[j] < days[i] + 7){
+                j++;
+            }
+            int cost7 = costs[1] + dp[j];
+
+            j = i;
+            while(j < n && days[j] < days[i] + 30){
+                j++;
+            }
+            int cost30 = costs[2] + dp[j];
+
+            dp[i] = min({cost1, cost7, cost30});
+        }
+
+        return dp[0];
     }
 };
