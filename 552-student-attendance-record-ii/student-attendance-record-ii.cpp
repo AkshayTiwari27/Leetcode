cef26b86b29c7292c2c9a2c26de49ff8cd703488
AkshayTiwari27 <131443493+AkshayTiwari27@users.noreply.github.com>
2025-03-19 00:33:51 +0530

    Time: 25 ms (92.99%) | Memory: 10.3 MB (92.11%) - LeetSync

--- 552-student-attendance-record-ii/student-attendance-record-ii.cpp
+++ 552-student-attendance-record-ii/student-attendance-record-ii.cpp
@@ -3,21 +3,28 @@ public:
     int dp[100001][2][3];
     int MOD = 1e9 + 7;
 
-    int solve(int n, int absence, int consecutive_late){
-        if(absence >=2 || consecutive_late >= 3) return 0;
-
-        if(n == 0) return 1;
-
-        if(dp[n][absence][consecutive_late] != -1) return dp[n][absence][consecutive_late];
-
-        int A = solve(n - 1, absence + 1, 0) % MOD;
-        int P = solve(n - 1, absence, 0) % MOD;
-        int L = solve(n - 1, absence, consecutive_late + 1) % MOD;
-
-        return dp[n][absence][consecutive_late] = ((A + P) % MOD + L) % MOD; 
-    }
     int checkRecord(int n) {
-        memset(dp, -1, sizeof(dp));
-        return solve(n, 0, 0);
+        memset(dp, 0, sizeof(dp));
+
+        for(int i = 0; i<2; i++){
+            for(int j = 0; j<3; j++){
+                dp[0][i][j] = 1;
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int A = 0; A <= 1; A++) {
+                for (int L = 0; L <= 2; L++) {
+
+                    long result = dp[i - 1][A][0]; // solve(n-1, absent, 0)
+                    result += (L + 1 <= 2 ? dp[i - 1][A][L + 1] : 0); // solve(n-1, A, L+1)
+                    result += (A + 1 <= 1 ? dp[i - 1][A + 1][0] : 0); // solve(n-1, A+1, 0)
+
+                    dp[i][A][L] = result % MOD;
+                }
+            }
+        }
+
+        return dp[n][0][0];
     }
 };
