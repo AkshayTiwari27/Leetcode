78981354568a46287377671da04b5fc9069fb840
AkshayTiwari27 <131443493+AkshayTiwari27@users.noreply.github.com>
2025-03-19 23:40:36 +0530

    Time: 3 ms (67.49%) | Memory: 17.1 MB (81.98%) - LeetSync

---
+++ 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    map<pair<int,int>, vector<TreeNode*>>mp;
+    vector<TreeNode*> solve(int start, int end){
+        if(start > end) return {NULL};
+
+        if(start == end){
+            TreeNode* root = new TreeNode(start);
+            return mp[{start, end}] ={root};
+        }
+
+        if(mp.count({start,end})) return mp[{start, end}];
+
+        vector<TreeNode*>result;
+        for(int i = start; i <= end; i++){
+            vector<TreeNode*>left_tree = solve(start, i-1);
+            vector<TreeNode*>right_tree = solve(i+1, end);
+
+            for(auto l: left_tree){
+                for(auto r: right_tree){
+                    TreeNode* root = new TreeNode(i);
+                    root->left = l;
+                    root->right = r;
+                    result.push_back(root);
+                }
+            }
+        }
+        return mp[{start, end}] = result;
+    }
+    vector<TreeNode*> generateTrees(int n) {
+        return solve(1, n);
+    }
+};
