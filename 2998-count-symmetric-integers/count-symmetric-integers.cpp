f6968307e4856c7ed59f3ce7fdbf576c298c0365
AkshayTiwari27 <131443493+AkshayTiwari27@users.noreply.github.com>
2025-04-11 14:58:58 +0530

    Time: 1 ms (97.10%) | Memory: 9.5 MB (84.15%) - LeetSync

--- 2998-count-symmetric-integers/count-symmetric-integers.cpp
+++ 2998-count-symmetric-integers/count-symmetric-integers.cpp
@@ -2,20 +2,17 @@ class Solution {
 public:
     int countSymmetricIntegers(int low, int high) {
         int count = 0;
-        for(int i = low; i<= high; i++){
-            string s = to_string(i);
-            int l = s.length();
-            if((l%2) != 0) continue;
 
-            int left = 0;
-            int right = 0;
-            for(int i = 0; i<l/2; i++){
-                left += s[i] - '0';
+        for(int i = low; i<=high; i++){
+            if(i>=10 && i<=99 && i%11 ==0) count++;
+            else if(i>=1000 && i<= 9999){
+                int first = i/1000;
+                int second = (i/100)%10;
+                int third = (i/10)%10;
+                int fourth = i%10;
+
+                if(first + second == third + fourth) count++;
             }
-            for(int j = l/2; j<l; j++){
-                right += s[j]- '0';
-            }
-            if(left == right) count++;
         }
 
         return count;
