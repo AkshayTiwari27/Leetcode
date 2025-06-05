36b6ba7c3667bf97eb0f8460da3a65113032fd57
AkshayTiwari27 <131443493+AkshayTiwari27@users.noreply.github.com>
2025-06-05 23:01:16 +0530

    Time: 1197 ms (13.57%) | Memory: 293.6 MB (12.06%) - LeetSync

---
+++ 15-3sum/3sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        set<vector<int>>st;
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i<n-1; i++){
+            int j = i+1; 
+            int k = n-1;
+            while(j<k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum == 0){
+                    st.insert({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+                }
+                else if(sum<0) j++;
+                else k--;
+            }
+        }
+
+        vector<vector<int>>ans;
+        for(auto& v: st){
+            ans.push_back(v);
+        }
+
+        return ans;
+    }
+};
