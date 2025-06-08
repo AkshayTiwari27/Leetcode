85e65483c99d3655e52e9f82019f1d9cfdf341a9
AkshayTiwari27 <131443493+AkshayTiwari27@users.noreply.github.com>
2025-06-08 15:42:14 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (45.73%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -6,8 +6,8 @@ public:
         while(lo<=hi){
             int mid = lo + (hi-lo)/2;
             if(nums[mid] == target) return mid;
-            else if(nums[mid] < target) lo++;
-            else hi--;
+            else if(nums[mid] < target) lo = mid+1;
+            else hi = mid-1;
         }
 
         return -1;
