275bb11a4754dba600d4a6f6a8201c07f1d05c38
AkshayTiwari27 <131443493+AkshayTiwari27@users.noreply.github.com>
2025-07-21 14:26:01 +0530

    Time: 31 ms (46.51%) | Memory: 44.4 MB (67.47%) - LeetSync

---
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string makeFancyString(string s) {
+        string result;
+        for(int i = 0; i<s.length(); i++){
+            if(i>= 2 &&s[i] == s[i-1] && s[i] == s[i-2]) continue;
+            result += s[i];
+        }
+
+        return result;
+    }
+};
