83cfb57f109dcbad4c0d74d06975f5c4614e3b19
AkshayTiwari27 <131443493+AkshayTiwari27@users.noreply.github.com>
2025-08-02 01:56:33 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (29.71%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     int dp[366];
-    int solve(int i, vector<int>& days, vector<int>& costs){
+    int solve(int i, vector<int>&days, vector<int>& costs){
         if(i >= days.size()) return 0;
         if(dp[i] != -1) return dp[i];
+
         int cost1 = costs[0] + solve(i+1, days, costs);
 
         int j = i;
-        int max_day = 7 + days[i];
-        while(j<days.size() && days[j] < max_day) j++;
+        int max_day = days[i] + 7;
+        while(j < days.size() && days[j] < max_day) j++;
         int cost2 = costs[1] + solve(j, days, costs);
 
-        
         j = i;
-        max_day = 30 + days[i];
-        while(j<days.size() && days[j] < max_day) j++;
+        max_day = days[i] + 30;
+        while(j < days.size() && days[j] < max_day) j++;
         int cost3 = costs[2] + solve(j, days, costs);
 
         return dp[i] = min({cost1, cost2, cost3});
-
+        
     }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         memset(dp, -1, sizeof(dp));
