4b67b626daadc9aa9571cc558d38755099c27561
AkshayTiwari27 <131443493+AkshayTiwari27@users.noreply.github.com>
2025-08-01 23:56:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (75.38%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    int dp[101];
-    int solve(int i, string &s){
-        if(i == s.length()) return 1;
-        if(s[i] == '0') return 0;
-
-        if(dp[i] != -1) return dp[i];
-
-        int result = solve(i+1, s);
-        if(i+1< s.length() && (s[i] == '1' || (s[i] == '2' && s[i+1] <= '6')))
-        result += solve(i+2, s);
-
-        return dp[i] = result;
-    }
+   
     int numDecodings(string s) {
-        memset(dp, -1, sizeof(dp));
-        return solve(0, s);
+        int n = s.length();
+        vector<int>dp(n+1);
+
+        dp[n] = 1;
+        for(int i = n-1; i>= 0; i--){
+            if(s[i] == '0') dp[i] = 0;
+            else{
+                int result = dp[i+1];
+                if(i+1< s.length() && (s[i] == '1' || (s[i] == '2' && s[i+1] <= '6')))
+                result += dp[i+2];
+
+                dp[i] = result;
+            }
+        }
+
+        return dp[0];
     }
 };
