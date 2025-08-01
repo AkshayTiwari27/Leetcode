7a2365b042b51c05a5cd8041274a52e7e5f07bee
AkshayTiwari27 <131443493+AkshayTiwari27@users.noreply.github.com>
2025-08-02 01:23:29 +0530

    Time: 55 ms (58.35%) | Memory: 9.2 MB (91.99%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -2,19 +2,16 @@ class Solution {
 public:
     int dp[10001];
     int solve(int n){
-        if(n == 0) return dp[n] = 0;
+        if(n == 0) return 0;
         if(n < 0) return 0;
-        
+
         if(dp[n] != -1) return dp[n];
 
-        int result = 0;
-        int mini = INT_MAX;
-        for(int i = 1; i*i<=n; i++){
-            result = solve(n - (i*i));
-            mini = min(result + 1, mini);
+        int result = INT_MAX;
+        for(int i = 1; i*i <=n; i++){
+            result = min(result, 1 + solve(n - (i*i)));
         }
-
-        return dp[n] = mini;
+        return dp[n] = result;
     }
     int numSquares(int n) {
         memset(dp, -1, sizeof(dp));
