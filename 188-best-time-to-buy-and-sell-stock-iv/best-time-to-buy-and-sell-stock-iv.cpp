828ce367449dbd58f7dbb14fefa83a7dc6f589b3
AkshayTiwari27 <131443493+AkshayTiwari27@users.noreply.github.com>
2025-08-04 01:23:10 +0530

    Time: 5 ms (68.22%) | Memory: 17.6 MB (64.24%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    int dp[1001][201];
-    int solve(int i, int trans_no, vector<int>&prices, int k){
-        if(i == prices.size() || trans_no == 2 * k) return 0;
-
-        if(dp[i][trans_no] != -1) return dp[i][trans_no];
-        int profit = 0;
-        if(trans_no %2 == 0) profit = max(-prices[i] + solve(i+1, trans_no+1, prices, k), solve(i+1, trans_no, prices, k));
-        else profit = max(prices[i] + solve(i+1, trans_no+1, prices, k), solve(i+1, trans_no, prices, k));
-
-        return dp[i][trans_no] = profit;
-    }
+    
     int maxProfit(int k, vector<int>& prices) {
-        memset(dp, -1, sizeof(dp));
-        return solve(0, 0, prices, k);
+        int n = prices.size();
+
+        vector<vector<int>>dp(n+1, vector<int>(((2*k) + 1), 0));
+
+        for(int i = n-1; i>= 0; i--){
+            for(int trans_no = 0; trans_no < 2*k; trans_no++){
+                int profit = 0;
+                if(trans_no % 2 == 0) profit = max(-prices[i] + dp[i+1][trans_no +1], dp[i+1][trans_no]);
+                else profit = max(prices[i] + dp[i+1][trans_no +1], dp[i+1][trans_no]);
+                dp[i][trans_no] = profit;
+            }
+        }
+
+        return dp[0][0];
     }
 };
