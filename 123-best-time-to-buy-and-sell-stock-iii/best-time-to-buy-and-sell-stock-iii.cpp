a4b7b9f9ea423817f122b79f617784ef4e84e89e
AkshayTiwari27 <131443493+AkshayTiwari27@users.noreply.github.com>
2025-08-04 00:14:14 +0530

    Time: 40 ms (77.99%) | Memory: 89.5 MB (69.42%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -2,10 +2,8 @@ class Solution {
 public:
     int dp[100001][2][3];
     int solve(int i, int buy, vector<int>&prices, int cap){
-        if(i == prices.size()) return 0;
-        if(cap == 0) return 0;
+        if(i == prices.size() || cap == 0) return 0;
         if(dp[i][buy][cap] != -1) return dp[i][buy][cap];
-
         int profit = 0;
         if(buy) profit = max(-prices[i] + solve(i+1, 0, prices, cap), solve(i+1, 1, prices, cap));
         else profit = max(prices[i] + solve(i+1, 1, prices, cap-1), solve(i+1, 0, prices, cap));
