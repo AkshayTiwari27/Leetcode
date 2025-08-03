b94adf9b36ca3c38083e69b26de0f1f33573aba9
AkshayTiwari27 <131443493+AkshayTiwari27@users.noreply.github.com>
2025-08-04 00:08:15 +0530

    Time: 19 ms (66.89%) | Memory: 58.8 MB (88.43%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -3,17 +3,18 @@ public:
     
     int maxProfit(vector<int>& prices, int fee) {
         int n = prices.size();
-        vector<vector<int>>dp(n+1, vector<int>(2, 0));
+        vector<int>curr(2,0), ahead(2,0);
 
         for(int i = n-1; i>=0; i--){
             for(int buy = 0; buy<=1; buy++){
                 int profit = 0;
-                if(buy) profit = max(-prices[i] + dp[i+1][0], dp[i+1][1]);
-                else profit = max(prices[i] - fee + dp[i+1][1], dp[i+1][0]);
-                dp[i][buy] = profit;
+                if(buy) profit = max(-prices[i] + ahead[0], ahead[1]);
+                else profit = max(prices[i] - fee + ahead[1], ahead[0]);
+                curr[buy] = profit;
             }
+            ahead = curr;
         }
 
-        return dp[0][1];
+        return ahead[1];
     }
 };
