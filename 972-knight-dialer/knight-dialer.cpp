21b1d1b20d05685b7b24c6d48301f3b73cf0b31a
AkshayTiwari27 <131443493+AkshayTiwari27@users.noreply.github.com>
2025-08-08 11:20:33 +0530

    Time: 45 ms (83.38%) | Memory: 9.1 MB (84.24%) - LeetSync

--- 972-knight-dialer/knight-dialer.cpp
+++ 972-knight-dialer/knight-dialer.cpp
@@ -4,32 +4,27 @@ public:
         {4,6} , {8,6}, {7,9}, {4,8}, {3,9,0}, {}, {7,1,0}, {2,6}, {1,3}, {2,4}
     };
     int MOD = 1e9 + 7;
-
     int dp[5001][11];
-    
-    int knightDialer(int n) {
-        memset(dp, -1, sizeof(dp));
-
-        for(int cell = 0; cell <= 9; cell++){
-            dp[0][cell] = 1;
-        }
-
-        for(int i = 1; i < n; i++ ){
-            for(int cell = 0; cell <= 9; cell++){
-                int ans = 0;
-                for(auto &v: adj[cell]){
-                    ans = (ans + dp[i-1][v] )% MOD;
-                }
-                dp[i][cell] = ans;
-            }
-        }
-
+    int solve(int n, int cell){
+        if(n == 0) return 1;
 
         int result = 0;
-        for(int i = 0; i <= 9; i++){
-            result = (result + dp[n-1][i]) % MOD;
+        if(dp[n][cell] != -1) return dp[n][cell];
+
+        for(auto & v : adj[cell]){
+            result =  (result + solve(n-1, v)) % MOD;
         }
-        return result;
-        
+
+        return dp[n][cell] = result % MOD;
+    }
+
+    int knightDialer(int n) {
+        int result = 0;
+        memset(dp, -1, sizeof(dp));
+        for(int cell = 0; cell <= 9; cell++){
+            result =  (result + solve(n-1, cell)) % MOD;
+        }
+
+        return result % MOD;
     }
 };
