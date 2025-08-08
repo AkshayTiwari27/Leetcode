590b5dfe6e5b586f4926c377d30b20dffb48550a
AkshayTiwari27 <131443493+AkshayTiwari27@users.noreply.github.com>
2025-08-08 22:06:13 +0530

    Time: 181 ms (67.30%) | Memory: 18.3 MB (75.42%) - LeetSync

--- 552-student-attendance-record-ii/student-attendance-record-ii.cpp
+++ 552-student-attendance-record-ii/student-attendance-record-ii.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
     int M = 1e9 + 7;
-    
+    int dp[100001][2][3];
+    int solve(int n, int a, int l){
+        if(a >=2 || l >=3) return 0;
+        if(n == 0) return 1;
+
+        if(dp[n][a][l] != -1) return dp[n][a][l];
+
+        int A = solve(n-1, a + 1, 0);
+        int P = solve(n-1, a, 0);
+        int L = solve(n-1, a, l+1);
+
+        return dp[n][a][l] = ((A+P)%M + L)%M;
+    }
     int checkRecord(int n) {
-        vector<vector<vector<int>>>dp(n+1, vector<vector<int>>(2, vector<int>(3,0)));
-
-        for(int a = 0; a <2; a++){
-            for(int l = 0; l< 3; l++){
-                dp[0][a][l] = 1;
-            }
-        }
-
-        for(int i = 1; i<=n; i++){
-            for(int a= 0; a<2; a++){
-                for(int l = 0; l<3; l++){
-                    long result = dp[i - 1][a][0]; 
-                    result += (l + 1 <= 2 ? dp[i - 1][a][l + 1] : 0); 
-                    result += (a + 1 <= 1 ? dp[i - 1][a + 1][0] : 0); 
-
-                    dp[i][a][l] = result % M;
-                }
-            }
-        }
-
-        return dp[n][0][0];
+        memset(dp, -1, sizeof(dp));
+        return solve(n, 0, 0) % M;
     }
 };
