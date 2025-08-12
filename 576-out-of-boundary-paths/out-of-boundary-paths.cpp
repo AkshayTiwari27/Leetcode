63c95d552e0fa795c336213e0e8c40808ccd0a9e
AkshayTiwari27 <131443493+AkshayTiwari27@users.noreply.github.com>
2025-08-12 11:32:06 +0530

    Time: 16 ms (14.05%) | Memory: 12.3 MB (28.19%) - LeetSync

--- 576-out-of-boundary-paths/out-of-boundary-paths.cpp
+++ 576-out-of-boundary-paths/out-of-boundary-paths.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
     int M = 1e9 + 7;
-    vector<vector<int>> directions = {{-1,0}, {0,-1}, {1,0}, {0,1}};
+    vector<vector<int>>directions = {{-1,0}, {0, -1}, {1, 0}, {0, 1}};
     
     int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
-        vector<vector<int>> prev(m, vector<int>(n, 0)), curr(m, vector<int>(n, 0));
+        vector<vector<vector<int>>>dp(m+1, vector<vector<int>>(n+1, vector<int>(maxMove+1, 0)));
 
-        for (int k = 1; k <= maxMove; k++) {
-            // Reset curr for the current move count
-            for (int i = 0; i < m; i++) {
-                fill(curr[i].begin(), curr[i].end(), 0);
-            }
 
-            for (int i = 0; i < m; i++) {
-                for (int j = 0; j < n; j++) {
-                    for (auto &dir : directions) {
+        for(int k = 1; k<= maxMove; k++){
+            for(int i = 0; i<m; i++){
+                for(int j = 0; j<n; j++){
+                    for(auto &dir :directions){
                         int nr = i + dir[0];
                         int nc = j + dir[1];
-                        
-                        if (nr < 0 || nc < 0 || nr >= m || nc >= n) {
-                            curr[i][j] = (curr[i][j] + 1) % M;
-                        } else {
-                            curr[i][j] = (curr[i][j] + prev[nr][nc]) % M;
+                        if (nr < 0 || nc < 0 || nr >= m || nc >= n){
+                            dp[i][j][k] = (dp[i][j][k]+1) % M;
+                        }
+                        else{
+                            dp[i][j][k] = (dp[i][j][k]+ dp[nr][nc][k-1]) % M;
                         }
                     }
                 }
             }
-            prev = curr;
         }
 
-        return prev[startRow][startColumn];
+        return dp[startRow][startColumn][maxMove];
     }
 };
